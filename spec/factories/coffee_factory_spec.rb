24ec998713e0d95fcb20bb3e11036694490ade5a
Leonardo Bernardellli <leobernardelli@gmail.com>
2018-01-20 21:55:30 -0200

    Refactor to follow code best practices

--- spec/factories/coffee_factory_spec.rb
+++ spec/factories/coffee_factory_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe CoffeeFactory do
   end
 
   context 'Build one coffee' do
-    let(:prices_json) {
+    let(:prices_json) do
       <<-JSON
       [
         { "drink_name": "short espresso", "prices": { "small": 3.03 } }
       ]
       JSON
-    }
+    end
 
     subject { CoffeeFactory.new(prices_json).build }
 
@@ -22,20 +22,20 @@ RSpec.describe CoffeeFactory do
       expect(subject.size).to be 1
       expect(subject[0].name).to eq('short espresso')
       expect(subject[0].sizes.size).to be 1
-      expect(subject[0].sizes[0]).eql?(CoffeeVariant.new({ size: :small, price: 3.03 }))
+      expect(subject[0].sizes[0]).eql?(CoffeeVariant.new(size: :small, price: 3.03))
     end
   end
 
 
   context 'Build two coffees' do
-    let(:prices_json) {
+    let(:prices_json) do
       <<-JSON
       [
         { "drink_name": "short espresso", "prices": { "small": 3.03 } },
         { "drink_name": "long black", "prices": { "small": 3.25, "medium": 3.50 } }
       ]
       JSON
-    }
+    end
 
     subject { CoffeeFactory.new(prices_json).build }
 
@@ -43,12 +43,12 @@ RSpec.describe CoffeeFactory do
     it 'A bunch of coffees' do
       expect(subject[0].name).to eq('short espresso')
       expect(subject[0].sizes.size).to be 1
-      expect(subject[0].sizes[0]).eql?(CoffeeVariant.new({ size: :small, price: 3.03 }))
+      expect(subject[0].sizes[0]).eql?(CoffeeVariant.new(size: :small, price: 3.03))
 
       expect(subject[1].name).to eq('long black')
       expect(subject[1].sizes.size).to be 2
-      expect(subject[1].sizes[0]).eql?(CoffeeVariant.new({ size: :small, price: 3.25 }))
-      expect(subject[1].sizes[1]).eql?(CoffeeVariant.new({ size: :medium, price: 3.50 }))
+      expect(subject[1].sizes[0]).eql?(CoffeeVariant.new(size: :small, price: 3.25))
+      expect(subject[1].sizes[1]).eql?(CoffeeVariant.new(size: :medium, price: 3.50))
     end
   end
 end
