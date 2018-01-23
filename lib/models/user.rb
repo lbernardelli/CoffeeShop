a908470a3ba0ad56518cf7ba887c76cb1a6cf48a
Leonardo Bernardellli <leobernardelli@gmail.com>
2018-01-23 00:34:15 -0200

    Implementing an user balance, to accomplish that I need to add a pay and total payed methods

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -6,6 +6,7 @@ class User
   def initialize(name:)
     @name = name
     @orders = []
+    @payments = []
   end
 
   def add_order(order)
@@ -14,6 +15,19 @@ class User
   end
 
   def total_ordered?
-    @orders.inject(0.00) { |sum, order| sum + order.total? }
+    @orders.inject(BigDecimal.new('0.00')) { |sum, order| BigDecimal.new(sum.to_s) + BigDecimal.new(order.total?.to_s) }
+  end
+
+  def pay(amount)
+    @payments << Payment.new(amount: amount)
+    self
+  end
+
+  def balance?
+    (BigDecimal.new(total_payed?.to_s) - BigDecimal.new(total_ordered?.to_s)).abs
+  end
+
+  def total_payed?
+    @payments.inject(BigDecimal.new('0.00')) { |sum, payment| BigDecimal.new(sum.to_s) + BigDecimal.new(payment.amount.to_s) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,36 +3,86 @@
 require 'spec_helper'
 
 RSpec.describe User do
+  let(:user) { User.new(name: 'Micheal') }
+
   describe 'total_ordered?' do
-    let(:user) { User.new(name: 'Micheal') }
+    subject { user.total_ordered? }
+
     context 'Empty order' do
-      subject { user }
-
-      it 'Total ordered is 0.00' do
-        expect(subject.total_ordered?).to be 0.00
-      end
+      it('Total ordered is 0.00') { is_expected.to eql BigDecimal.new('0.00') }
     end
 
-    context 'User has ordered 1 product' do
-      subject do
-        user.add_order(create_order_double(1.00))
-      end
+    context 'User ordered 1 product' do
+      before { user.add_order(create_order_double(1.00)) }
 
-      it('had ordered a total of 1.00') { expect(subject.total_ordered?).to be 1.00 }
+      it('ordered a total of 1.00') { is_expected.to eql BigDecimal.new('1.00') }
     end
 
-    context 'User had ordered a bunch of products' do
-      subject do
+    context 'User ordered a bunch of products' do
+      before do
         user.add_order(create_order_double(1.00))
         user.add_order(create_order_double(2.25))
         user.add_order(create_order_double(3.50))
       end
 
-      it('has a total of 7.00') { expect(subject.total_ordered?).to be 6.75 }
+      it('has a total of 7.00') { is_expected.to eql BigDecimal.new('6.75') }
+    end
+  end
+
+  describe 'total_payed?' do
+    subject { user.total_payed? }
+
+    context 'No payments' do
+      it('payed a total of 0.00') { is_expected.to eql BigDecimal.new('0.00') }
     end
 
-    def create_order_double(price)
-      object_double(Order.new, total?: price)
+    context 'User payed 1.00' do
+      before { user.pay(1.00) }
+
+      it('payed a total of 1.00') { is_expected.to eql BigDecimal.new('1.00') }
     end
+
+    context 'User had ordered a bunch of products' do
+      before do
+        user.pay(1.00)
+        user.pay(2.5)
+        user.pay(3.53)
+      end
+
+      it('has a total of 7.03') { is_expected.to eql BigDecimal.new('7.03') }
+    end
+  end
+
+  describe 'balance' do
+    subject { user.balance? }
+
+    context 'No orders nor payments' do
+      it('has a balance of 0.00') { is_expected.to eql BigDecimal.new('0.00') }
+    end
+
+    context 'User had ordered 1 product' do
+      before { user.add_order(create_order_double(1.00)) }
+
+      it('has a balance of 1.00') { is_expected.to eql BigDecimal.new('1.00') }
+    end
+
+    context 'User payed 1.00' do
+      before { user.pay(1.00) }
+
+      it('has a balance of 1.00') { is_expected.to eql BigDecimal.new('1.00') }
+    end
+
+    context 'User ordered 1.00 and payed 1.00' do
+      before do
+        user.add_order(create_order_double(1.00))
+        user.pay(1.00)
+      end
+
+      it('has a balance of 0.00') { is_expected.to eql BigDecimal.new('0.00') }
+    end
+  end
+
+  def create_order_double(price)
+    object_double(Order.new, total?: price)
   end
 end
