a30212d6aafc9e5d54277785bc4d566169b2c971
Leonardo Bernardellli <leobernardelli@gmail.com>
2018-01-23 20:51:13 -0200

    Fixing user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,8 +39,7 @@ RSpec.describe User do
     context 'User payed 1.00' do
       before { user.pay(1.00) }
 
-      # User has a 1.00 credit
-      it('payed a total of -1.00') { is_expected.to eql BigDecimal.new('-1.00') }
+      it('payed a total of 1.00') { is_expected.to eql BigDecimal.new('1.00') }
     end
 
     context 'User had ordered a bunch of products' do
@@ -70,7 +69,8 @@ RSpec.describe User do
     context 'User payed 1.00' do
       before { user.pay(1.00) }
 
-      it('has a balance of 1.00') { is_expected.to eql BigDecimal.new('1.00') }
+      # User has a 1.00 credit
+      it('has a balance of -1.00') { is_expected.to eql BigDecimal.new('-1.00') }
     end
 
     context 'User ordered 1.00 and payed 1.00' do
