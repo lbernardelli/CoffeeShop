b7d91aa20d4de245092be2e450fca799bd357bd5
Leonardo Bernardellli <leobernardelli@gmail.com>
2018-01-23 22:54:50 -0200

    cleaning boilerplate code

--- lib/coffee_app.rb
+++ lib/coffee_app.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 require 'coffee_app/factories/coffee_factory'
-require 'coffee_app/models/coffee'
-require 'coffee_app/models/coffee_variant'
-require 'coffee_app/models/order'
-require 'coffee_app/models/user'
-require 'coffee_app/models/order_item'
-require 'coffee_app/models/payment'
 require 'coffee_app/utils/json_parser'
 require 'coffee_app/order_manager'
 require 'coffee_app/serializers/result_serializer'
 require 'bigdecimal'
+
+Dir[File.expand_path('../coffee_app/models/*.rb', __FILE__)].map do |path|
+  require path
+end
 
 module CoffeeApp
   def self.call(prices, orders, payments)
